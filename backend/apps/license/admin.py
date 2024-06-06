b313f3a53fc216276dd1e15fffe17a311ab6ed92
Backend <it@tsvs.gov.kg>
2024-06-06 14:36:28 +0600

    up:04

--- backend/apps/license/admin.py
+++ backend/apps/license/admin.py
@@ -7,12 +7,12 @@ from backend.apps.license.models import QuantitySchool
 
 @admin.register(Region)
 class RegionAdmin(admin.ModelAdmin):
-    list_display = ('title', 'is_active', 'created_date', 'update_date')
+    list_display = ('title', 'is_active', 'created_date', 'updated')
 
 @admin.register(IssuingAuthority)
 class IssuingAuthorityAdmin(admin.ModelAdmin):
-    list_display = ('title', 'is_active', 'created_date', 'update_date')
+    list_display = ('title', 'is_active', 'created_date', 'updated')
 
 @admin.register(QuantitySchool)
 class QuantitySchoolAdmin(admin.ModelAdmin):
-    list_display = ('title', 'is_active', 'created_date', 'update_date')
+    list_display = ('title', 'is_active', 'created_date', 'updated')
