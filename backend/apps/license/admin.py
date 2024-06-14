0d38f3464877c7b3200ed12c6e62efb69ae6d259
jaman-bala <ermekoffdastan@yandex.ru>
2024-06-14 10:47:04 +0600

    up:10.02

--- backend/apps/license/admin.py
+++ backend/apps/license/admin.py
@@ -3,10 +3,15 @@ from django.contrib import admin
 from backend.apps.license.models import IssuingAuthority
 from backend.apps.license.models import Region
 from backend.apps.license.models import DBLicense
+from backend.apps.license.models import CodeLicense
 
 
 @admin.register(Region)
 class RegionAdmin(admin.ModelAdmin):
+    list_display = ('title', 'is_active', 'created_date', 'updated')
+
+@admin.register(IssuingAuthority)
+class IssuingAuthorityAdmin(admin.ModelAdmin):
     list_display = ('title', 'is_active', 'created_date', 'updated')
 
 @admin.register(IssuingAuthority)
